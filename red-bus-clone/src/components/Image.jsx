43166dadfb93c63948529fd2b018f9c567534209
anandece348 <anandece2608@gmail.com>
2023-02-01 11:01:37 +0530

    commit

---
+++ red-bus-clone/src/components/Image.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Details from "./Details";
+// import "../styles/App.css";
+import "../App.css";
+
+export const Image = () => {
+  return (
+    <>
+      <div id="image">
+        <img src="https://seeklogo.com/images/R/redbus-logo-13648C0E43-seeklogo.com.png" alt="" />
+      </div>
+      <Details />
+    </>
+  );
+};
